398813b4b9b499bb694d25328c350c6c321c3a81
Sahith posa <sahith.posa1997@gmail.com>
2023-09-16 08:29:36 +0530

    terraform

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -5,7 +5,12 @@ terraform {
       version = "5.15.0"
     }
   }
-
+  backend "s3" {
+    bucket = "sahith-remote-state"
+    key = "foreach-demo"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-lock"
+  }
   
 
 }
